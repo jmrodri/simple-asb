d5c12abede159714f0a477b1f82bc499205ddbbf
jesus m. rodriguez <jmrodri@gmail.com>
2017-12-04 14:52:16 -0500

    Remove some of the variables to make it more concise

--- install_broker.sh
+++ install_broker.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 _dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-NAMESPACE="ansible-service-broker"
+# basically what the blog does
+
+oc cluster up --service-catalog=true
 
 oc login -u system:admin
-oc new-project ${NAMESPACE}
+oc new-project ansible-service-broker
 
 VARS="-p BROKER_CA_CERT=$(oc get secret -n kube-service-catalog -o go-template='{{ range .items }}{{ if eq .type "kubernetes.io/service-account-token" }}{{ index .data "service-ca.crt" }}{{end}}{{"\n"}}{{end}}' | tail -n 1)"
 
 oc process -f ${_dir}/deploy-ansible-service-broker.template.yaml \
-  -n ${NAMESPACE} $VARS | oc create -f -
+  -n "ansible-service-broker" $VARS | oc create -f -
 
 if [[ "$?" -ne 0 ]]; then
   echo "Error deploying ansible-service-broker"
